75a675e0fe91cecbfd1955eb7fb71fc95478e5c3
Luiz oth. <122753099+luizww@users.noreply.github.com>
2026-08-20 22:11:19 -0300

    Add src/main/java/br/com/loth/financeiro/model/Lancamento.java

---
+++ src/main/java/br/com/loth/financeiro/model/Lancamento.java
@@ -0,0 +1,30 @@
+package br.com.loth.financeiro.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Lancamento {
+    private Long id;
+    private String descricao;
+    private BigDecimal valor;
+    private TipoLancamento tipo;
+    private String categoria;
+    private LocalDate data;
+
+    public Lancamento(Long id, String descricao, BigDecimal valor, TipoLancamento tipo,
+                      String categoria, LocalDate data) {
+        this.id = id;
+        this.descricao = descricao;
+        this.valor = valor;
+        this.tipo = tipo;
+        this.categoria = categoria;
+        this.data = data;
+    }
+
+    public Long getId() { return id; }
+    public String getDescricao() { return descricao; }
+    public BigDecimal getValor() { return valor; }
+    public TipoLancamento getTipo() { return tipo; }
+    public String getCategoria() { return categoria; }
+    public LocalDate getData() { return data; }
+}
