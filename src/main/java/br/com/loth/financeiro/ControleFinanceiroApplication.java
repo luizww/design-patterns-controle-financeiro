535109144bc45ae5e0ff35a78f15b41af4b94297
Luiz oth. <122753099+luizww@users.noreply.github.com>
2026-08-20 22:11:11 -0300

    Add src/main/java/br/com/loth/financeiro/ControleFinanceiroApplication.java

---
+++ src/main/java/br/com/loth/financeiro/ControleFinanceiroApplication.java
@@ -0,0 +1,12 @@
+package br.com.loth.financeiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleFinanceiroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ControleFinanceiroApplication.class, args);
+    }
+}
